9bdb6b468d1c3f1fcaa7bc5da7f53c55c9f55326
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-03-21 18:44:53 -0700

    better commenting

--- demo/demo.js
+++ demo/demo.js
@@ -1,22 +1,4 @@
-
-// An emscripten-generated a.out.js can be simply included as another
-// script on a series ( <script src="../a.out.js"> </script> ), and then its
-// guts are available via Module.cwrap(), for eaxmple.
-//
-// var repair =  Module.cwrap('repairFile', 'number', ['string', 'string']);
-// repair('/working/input.webm', '/working/output.webm')
-//
-// where 'repairFile' should be exported at emcc compile time like
-//  -s EXPORTED_FUNCTIONS="['_repairFile']"
-//
-// https://kripken.github.io/emscripten-site/docs/api_reference/module.html
-
-// Here we'd like to use a node.js approach.  For that, there's a post.js
-// file where a single line
-//  module.exports.LibWebm = Module;
-// is added, to facilitate exporting of (all) symbols.
 var libwebm = require('../a.out.js').LibWebm;
-
 
 var seven = libwebm._saySeven();
 
--- post.js
+++ post.js
@@ -1,1 +1,17 @@
+// An emscripten-generated a.out.js can be simply included as another
+// script on a series ( <script src="../a.out.js"> </script> ), and then its
+// guts are available via Module.cwrap(), for eaxmple.
+//
+// var repair =  Module.cwrap('repairFile', 'number', ['string', 'string']);
+// repair('/working/input.webm', '/working/output.webm')
+//
+// where 'repairFile' should be exported at emcc compile time like
+//  -s EXPORTED_FUNCTIONS="['_repairFile']"
+//
+// https://kripken.github.io/emscripten-site/docs/api_reference/module.html
+
+// Here we'd like to use a node.js approach.  For that, there's a post.js file
+// where the single line below  is added, to facilitate exporting of (all)
+// symbols.
+
 module.exports.LibWebm = Module;
